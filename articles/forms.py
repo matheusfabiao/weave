12b3e79389c3d643c349d0559f59c721f25899eb
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-09-26 15:26:48 -0300

    feat: new article form template design

--- articles/forms.py
+++ articles/forms.py
@@ -7,6 +7,18 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'resume', 'content', 'tags']
+        labels = {
+            'title': 'Título',
+            'resume': 'Resumo',
+            'content': 'Conteúdo',
+            'tags': 'Palavras-Chave',
+        }
+        widgets = {
+            'title': forms.TextInput({'class': 'form-control'}),
+            'resume': forms.Textarea({'class': 'form-control'}),
+            'content': forms.Textarea({'class': 'form-control'}),
+            'tags': forms.SelectMultiple({'class': 'form-control'}),
+        }
 
 
 class CommentForm(forms.Form):
