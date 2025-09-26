c1c41141911f437cc93cd81c189bb8326277307d
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-09-26 10:41:48 -0300

    fix: added ready method to register signals in accounts/apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    @staticmethod
+    def ready():
+        import accounts.signals  # noqa
