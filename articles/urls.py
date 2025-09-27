9ee7b6daede72355d6902b2397799f3285ba3e8e
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-09-26 22:52:38 -0300

    style: changed some urls name for better pattern

--- articles/urls.py
+++ articles/urls.py
@@ -13,8 +13,8 @@ from .views import (
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-    path('new/', ArticleCreateView.as_view(), name='article_new'),
-    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('create/', ArticleCreateView.as_view(), name='article_create'),
+    path('<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
     path(
         '<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'
     ),
