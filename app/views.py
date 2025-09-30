2a06513dfdf403533ec2cf9c2a98ce0d5a0d1705
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-09-30 11:40:44 -0300

    feat: trending articles now displayed in home page

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.cache import cache_page
 from django.views.generic import ListView
 
 from articles.models import Article
+from articles.services import get_trending
 
 
 @method_decorator(cache_page(60), name='dispatch')
@@ -10,6 +11,11 @@ class HomeView(ListView):
     model = Article
     template_name = 'home.html'
     context_object_name = 'articles'
+    
+    def get_context_data(self, **kwargs):  # noqa
+        context = super().get_context_data(**kwargs)
+        context['trending'] = get_trending()
+        return context
 
     @staticmethod
     def get_queryset():
